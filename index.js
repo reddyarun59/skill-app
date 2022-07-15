180cee9629ed9aa905b0698156533de67a7d35e8
reddyarun59 <reddyarun59@yahoo.com>
2022-07-15 09:13:56 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require('express')
+const dotenv = require('dotenv').config()
+
+const port=process.env.PORT || 5000;
+
+const app=express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended:true }))
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`)
+})
